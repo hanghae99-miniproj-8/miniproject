a96ffaab8faaa2eb3c4cb525e866ee1209c920be
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-01-10 19:21:26 +0900

    W2 end
    2주차 강의 끝

--- Main/app.py
+++ Main/app.py
@@ -12,7 +12,9 @@ db = client.dbsparta_plus_week2
 @app.route('/')
 def main():
     # DB에서 저장된 단어 찾아서 HTML에 나타내기
-    return render_template("index.html")
+    msg = request.args.get("msg")
+    words = list(db.words.find({},{"_id" : False}))
+    return render_template("index.html",words = words,msg = msg)
 
 
 @app.route('/detail/<keyword>')
@@ -20,6 +22,9 @@ def detail(keyword):
     # API에서 단어 뜻 찾아서 결과 보내기
     status_receive = request.args.get("status_give")
     r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token 54c8a25ffc9d2460bc54412c0b7261225875abd1"})
+    #제대로 받을 경우는 무조건 200
+    if r.status_code != 200:
+        return redirect(url_for("main", msg = "단어가 이상행.."))
     result = r.json()
     print(result)
     return render_template("detail.html", word=keyword, result = result, status = status_receive)
