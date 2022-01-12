6166e4841d47eb2bc7613223e64b5f36fbf47fa4
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-01-12 14:00:53 +0900

    login
    login

--- mainPage/app.py
+++ mainPage/app.py
@@ -2,34 +2,114 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 import requests
 
-
+import jwt
+import datetime
+import hashlib
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
+#jinja2에서 break 사용하기 위한 라이브러리
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+
+#기본 프로필 사진 업로드
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+#로그인 secret key
+SECRET_KEY = 'SPARTA'
+
 
 client = MongoClient('3.34.190.40', 27017, username="test", password="test")
 db = client.miniproj
 
 
 @app.route('/')
+def home():
+    return render_template("login.html")
+
+@app.route('/login')
 def login():
-    # DB에서 저장된 단어 찾아서 HTML에 나타내기
-    return render_template("main.html")
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
 
 
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    nick_receive = request.form['nick_give']
+    doc = {
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
+        "nick": nick_receive,
+        "profile_pic": "",  # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png",  # 프로필 사진 기본 이미지
+        "profile_info": ""  # 프로필 한 마디
+
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+######
 @app.route('/main')
 def main():
-    id = request.args.get("id")
-    tag = request.args.get("tag")
-    print(tag)
-    if(tag == None):
-        post = list(db.review.find({}, {"_id": False}))
-    else:
-        post = list(db.review.find({"tag": {"$regex": tag}}, {"_id": False}))
-    like = list(db.like.find({'id': id}, {"_id": False}))
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
-    return render_template("main.html",id=id, post = post, like = like)
+        user_info = db.users.find_one({"username": payload["id"]})
+
+        id = (user_info['username'])
+        tag = request.args.get("tag")
+
+        if (tag == None):
+            post = list(db.review.find({}, {"_id": False}))
+        else:
+            post = list(db.review.find({"tag": {"$regex": tag}}, {"_id": False}))
+        like = list(db.like.find({'id': id}, {"_id": False}))
+
+        return render_template("main.html", id=id, post=post, like=like)
+
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+
 
 
 @app.route('/create')
