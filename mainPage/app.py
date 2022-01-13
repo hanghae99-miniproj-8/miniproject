107c384be44da14d462bf30db1350be6697e7320
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-01-13 13:43:41 +0900

    주석 추가

--- mainPage/app.py
+++ mainPage/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 import requests
-
 import jwt
 import datetime
 import hashlib
@@ -36,34 +35,46 @@ def login():
     return render_template('login.html', msg=msg)
 
 
+# 로그인
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
-    # 로그인
+
+    #로그인 정보 receive
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
 
+    #받은 pw hash 암호화와
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+
+    #유저 ID정보 확인
     result = db.users.find_one({'username': username_receive, 'password': pw_hash})
 
+    #ID정보가 DB에 존재
     if result is not None:
         payload = {
             'id': username_receive,
             'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 0.5)  # 로그인 30분 유지
         }
+
+        #토큰 암호화
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
         return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
+    # 조회 실패할 경우
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
+#회원 가입
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
+    #유저의 ID, PW, nick receive
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
-    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     nick_receive = request.form['nick_give']
+    #받은 PW hash 암호화
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+
     doc = {
         "username": username_receive,  # 아이디
         "password": password_hash,  # 비밀번호
@@ -74,57 +85,54 @@ def sign_up():
         "profile_info": ""  # 프로필 한 마디
 
     }
+    #db에 저장 후 결과 전달
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
 
-
+#ID 중복 체크
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
+    #ID를 받아 DB에서 존재하는지 여부를 전달
     username_receive = request.form['username_give']
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
-
-######
+#메인 화면
 @app.route('/main')
 def main():
+    #토큰 receive
     token_receive = request.cookies.get('mytoken')
     try:
+        #payload 선언
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
+        #db에서 알맞는 회원 정보를 user_info에 초기화
         user_info = db.users.find_one({"username": payload["id"]})
 
+        #전달 정보 초기화
+        all_user_info = list(db.users.find({},{'_id':0}))
         id = (user_info['username'])
         tag = request.args.get("tag")
 
-        all_user_info = list(db.users.find({},{'_id':0}))
-
-
-        if (tag == None):
+        #search 정보
+        if (tag == None):   #search 정보가 없을 경우
             post = list(db.review.find({}, {"_id": False}))
-        else:
+        else:               #search 정보가 있을 경우
             post = list(db.review.find({"tag": {"$regex": tag}}, {"_id": False}))
-        like = list(db.like.find({'id': id}, {"_id": False}))
 
-        return render_template("main.html", id=id, post=post, like=like, user_info=user_info,all_user_info = all_user_info)
+        return render_template("main.html", id=id, post=post, user_info=user_info,all_user_info = all_user_info)
 
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
-
-
-
-
+#게시물 포스트 페이지
 @app.route('/create')
 def create():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
         user_info = db.users.find_one({"username": payload["id"]})
-
         id = (user_info['username'])
 
         return render_template("create.html", id=id)
@@ -134,12 +142,10 @@ def create():
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
-
-
-
-
+#게시물 포스팅
 @app.route('/create/save', methods=['POST'])
 def save():
+    #포스팅 정보 receive
     user_receive = request.form["user_give"]
     title_receive = request.form["title_give"]
     add_receive = request.form["add_give"]
@@ -147,7 +153,7 @@ def save():
     desc_receive = request.form["desc_give"]
     tag_receive = request.form["tag_give"]
 
-
+    #포스팅 정보 초기화
     doc={
         'id' : user_receive,
         'title' : title_receive,
@@ -158,47 +164,53 @@ def save():
         'like' : []
 
     }
+    #DB insert
     db.review.insert_one(doc)
 
     return jsonify({'result': 'success', 'msg': '입력 완료'})
 
+#좋아요
 @app.route('/main/like', methods=['POST'])
 def like_post():
+    #좋아요 누른 계정과 제목 receive
     id_receive = request.form["id_give"]
     title_receive = request.form["title_give"]
 
+    #포스트의 like에 누른 계정 추가 후 업데이트
     post = db.review.find_one({'title' : title_receive}, {"_id": False})
     post['like'].append(id_receive)
     db.review.update_one({'title': title_receive},{'$set': {'like' : post['like']}})
 
     return jsonify({'result': 'success', 'msg': 'Like it!'})
 
-
+#좋아요 취소
 @app.route('/main/unlike', methods=['POST'])
 def unlike_post():
+    # 좋아요 누른 계정과 제목 receive
     id_receive = request.form["id_give"]
     title_receive = request.form["title_give"]
 
+    # 포스트의 like에 누른 계정 제거 후 업데이트
     post = db.review.find_one({'title' : title_receive}, {"_id": False})
     post['like'].remove(id_receive)
     db.review.update_one({'title': title_receive},{'$set': {'like' : post['like']}})
 
     return jsonify({'result': 'success', 'msg': 'Cancle Like...'})
 
-
+#포스트 상세 페이지
 @app.route('/detail')
 def detail():
+    #포스트의 제목 receive
     title = request.args.get("title")
+    #토큰 receive
     token_receive = request.cookies.get('mytoken')
     try:
 
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
         user_info = db.users.find_one({"username": payload["id"]})
 
         id = (user_info['username'])
         post = list(db.review.find({"title": title}, {"_id": False}))
-        print(post)
 
         return render_template("detail.html", id=id, post = post)
 
@@ -208,17 +220,14 @@ def detail():
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
     return render_template("detail.html")
 
+#유저 정보 수정 페이지
 @app.route('/user')
 def user():
-    title = request.args.get("title")
+
     token_receive = request.cookies.get('mytoken')
     try:
-
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
         user_info = db.users.find_one({"username": payload["id"]})
-
-
 
         return render_template("user.html", id=id, user_info = user_info )
 
@@ -228,20 +237,24 @@ def user():
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
     return render_template("detail.html")
 
+#계정 프로필 사진 변경
 @app.route('/user/update_profile', methods=['POST'])
 def save_img():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        #변경점 receive
         username = payload["id"]
         name_receive = request.form["name_give"]
         nick_receive = request.form["nick_give"]
         about_receive = request.form["about_give"]
+
         new_doc = {
             "profile_name": name_receive,
             "nick": nick_receive,
             "profile_info": about_receive
         }
+        #이미지 파일 경로 설정 후 저장
         if 'file_give' in request.files:
             file = request.files["file_give"]
             filename = secure_filename(file.filename)
@@ -254,7 +267,6 @@ def save_img():
         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
